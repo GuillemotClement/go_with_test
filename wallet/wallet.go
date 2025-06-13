dd0d39c590f18fb6ee81867038f69357920d3c2d
GuillemotClement <guillemot.clement@protonmail.com>
2025-06-13 23:34:11 +0200

    add test and method before use pointer

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,1 +1,16 @@
 package wallet
+
+type Wallet struct {
+	balance int
+}
+
+// dans une fonction, on passe une copie de la struct
+// la methode ne modifie pas la valeur de la struct
+// w est une copie de la struct
+func (w Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+
+func (w Wallet) Balance() int {
+	return w.balance
+}
