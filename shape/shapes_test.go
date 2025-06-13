b38e1f529bbcee91e8de855f47f7957de52a62ea
GuillemotClement <guillemot.clement@protonmail.com>
2025-06-13 23:25:59 +0200

    show struct in test

--- shape/shapes_test.go
+++ shape/shapes_test.go
@@ -15,20 +15,27 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	// on definis une table driven test qui permet de passer une liste de test qui sera tester de la meme maniere
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
 		// on definis les test a realiser dans une struct anonyme
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		// on peut ajouter le nom pour plus de lisibiliter avant de passer la valeur
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		// tt.name depuis le case permet d'affiche le nom de la struct
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				// %#v pour afficher la struct qui merde
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
 	}
 
 	// TEST SANS LA TABLE DE TEST
