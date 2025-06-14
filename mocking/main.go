33ede1ac71a8473f942a253e31a9c45a0456bc52
GuillemotClement <guillemot.clement@protonmail.com>
2025-06-14 13:06:16 +0200

    write on stdout

--- mocking/main.go
+++ mocking/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func main() {
-	//Countdown()
+// V1
+// on passe en parametre de la fonction un pointeur vers le buffer qui sera utiliser pour stocker ce qu'on ecrit
+//func Countdown(out *bytes.Buffer) {
+//	fmt.Fprint(out, "3")
+//}
+
+// V2
+// on rend la fonction generique en lui passant une interface io.Writter
+// cela permet de rendre la fonction generique et lui permettant d'accepter n'importe quel objet qui implement l'interface (pour ecriture)
+func Countdown(out io.Writer) {
+	fmt.Fprint(out, "3")
 }
 
-// on passe en parametre de la fonction un pointeur vers le buffer qui sera utiliser pour stocker ce qu'on ecrit
-func Countdown(out *bytes.Buffer) {
-	fmt.Fprint(out, "3")
+func main() {
+	// os.Stdout permet d'ecrire dans le terminal (la sortie standard)
+	Countdown(os.Stdout)
 }
