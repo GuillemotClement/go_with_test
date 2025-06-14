0b821bef2941087ba84f87060539276046444e69
GuillemotClement <guillemot.clement@protonmail.com>
2025-06-14 11:13:17 +0200

    handler unknow work

--- dictionnaire/dictionary_test.go
+++ dictionnaire/dictionary_test.go
@@ -3,10 +3,27 @@ package dictionnaire
 import "testing"
 
 func TestSearsh(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
-	got := dictionary.Search("test")
-	want := "this is just a test"
-	assertStrings(t, got, want)
+	dictionary := Dictionary{"test": "this is juste a test"}
+
+	t.Run("know word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is juste a test"
+
+		assertStrings(t, got, want)
+
+	})
+
+	t.Run("unknow word", func(t *testing.T) {
+		_, err := dictionary.Search("unknow")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatalf("expected to get an error")
+		}
+		// fonction Error() pour afficher une string pour le message d'erreur
+		assertStrings(t, err.Error(), want)
+	})
+
 }
 
 func assertStrings(t testing.TB, got, want string) {
--- dictionnaire/dictionnary.go
+++ dictionnaire/dictionnary.go
@@ -1,7 +1,15 @@
 package dictionnaire
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	// premiere valeur est celle retourner
+	// second est une valeur boolean qui indique si la valeur existe
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition, nil
 }
