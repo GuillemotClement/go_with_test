a8e20c16d9eae19413eb0293bc1380a5c4c4e125
GuillemotClement <guillemot.clement@protonmail.com>
2025-06-14 16:45:13 +0200

    implemented Server HTTP

---
+++ Application/server_integration_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRecordingWinsAndRetrivingThem(t *testing.T) {
+	// on creer les deux composants
+	store := NewInMemoryPlayerStore()
+	server := PlayerServer{store}
+	player := "Pepper"
+	// on enregistre 3 victoire pour user
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+
+	response := httptest.NewRecorder()
+	// on fait une requete pour retrouver le nombre de victoire enregistrer
+	server.ServeHTTP(response, newGetScoreRequest(player))
+	// on test le status code retourner
+	assertStatus(t, response.Code, http.StatusOK)
+	// on test la reponse
+	assertResponseBody(t, response.Body.String(), "3")
+}
