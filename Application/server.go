ce030cba246321877f88f605f6efc8125d5da23a
GuillemotClement <guillemot.clement@protonmail.com>
2025-06-14 16:03:38 +0200

    refacto ServerHttp

--- Application/server.go
+++ Application/server.go
@@ -17,6 +17,16 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// on verifie la methode de la request
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
 	score := p.store.GetPlayerScore(player)
@@ -26,6 +36,39 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprint(w, score)
 }
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
+}
+
+//func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//
+//	// on verifie la methode de la request
+//	if r.Method == http.MethodPost {
+//		w.WriteHeader(http.StatusAccepted)
+//		return
+//	}
+//
+//	player := strings.TrimPrefix(r.URL.Path, "/players/")
+//
+//	score := p.store.GetPlayerScore(player)
+//
+//	if score == 0 {
+//		w.WriteHeader(http.StatusNotFound)
+//	}
+//	fmt.Fprint(w, score)
+//}
+
+//func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//	player := strings.TrimPrefix(r.URL.Path, "/players/")
+//
+//	score := p.store.GetPlayerScore(player)
+//
+//	if score == 0 {
+//		w.WriteHeader(http.StatusNotFound)
+//	}
+//	fmt.Fprint(w, score)
+//}
 
 //func PlayerServer(w http.ResponseWriter, r *http.Request) {
 //	// on recupere le joueur depuis l'url
