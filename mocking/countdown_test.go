cab865fd6b1a7e2ec62b3e47a5eac3ce1b426852
GuillemotClement <guillemot.clement@protonmail.com>
2025-06-14 13:17:56 +0200

    add sleeper and mock

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -8,8 +8,10 @@ import (
 func TestCountdown(t *testing.T) {
 	// on creer un nouveau buffer => une zone memoire temporaire
 	buffer := &bytes.Buffer{}
+	// on ajoute l'espion pour connaitre le nombre d'appel
+	spySleeper := &SpySleeper{}
 	// on passe ce buffer dnas la fonction => le pointeur et permet a la fonction de modifier le buffer
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 	// en interne, le buffer contiens des []byte => c'est un tableau de bytes
 	// avec .String() on convertis le []byte en string
 	got := buffer.String()
@@ -20,5 +22,9 @@ Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
 	}
 }
--- mocking/main.go
+++ mocking/main.go
@@ -9,9 +9,28 @@ import (
 
 // V1
 // on passe en parametre de la fonction un pointeur vers le buffer qui sera utiliser pour stocker ce qu'on ecrit
-//func Countdown(out *bytes.Buffer) {
-//	fmt.Fprint(out, "3")
-//}
+//
+//	func Countdown(out *bytes.Buffer) {
+//		fmt.Fprint(out, "3")
+//	}
+type Sleeper interface {
+	Sleep()
+}
+
+// permet de connaitre le nombre d'appel de la fonction sleep()
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
 
 // definition des constante
 const finalWorld = "Go!"
@@ -20,17 +39,18 @@ const countDownStart = 3
 // V2
 // on rend la fonction generique en lui passant une interface io.Writter
 // cela permet de rendre la fonction generique et lui permettant d'accepter n'importe quel objet qui implement l'interface (pour ecriture)
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	// boucle qui permet de faire le decompte
 	for i := countDownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		// permet d'ajoute un temps entre chaque iteration
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWorld)
 }
 
 func main() {
+	sleeper := &DefaultSleeper{}
 	// os.Stdout permet d'ecrire dans le terminal (la sortie standard)
-	Countdown(os.Stdout)
+	Countdown(os.Stdout, sleeper)
 }
